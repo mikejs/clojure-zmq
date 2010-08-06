b9659006dd242cdfdbbd488f394ec4d996a860c6
Michael Stephens <michaeljste@gmail.com>
2010-08-06 10:39:15 -0400

    use native-deps for jzmq

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject clojure-zmq "2.0.7-SNAPSHOT"
   :description "A Clojure binding/wrapper for ZeroMQ"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
-                 [org.clojars.mikejs/jzmq "2.0.7-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+  ; jzmq-native-deps only has OS X x86_64 support at the moment
+  :native-dependencies [[org.clojars.mikejs/jzmq-native-deps
+                         "2.0.7-SNAPSHOT"]]
+  :dev-dependencies [[native-deps "1.0.1"]])
