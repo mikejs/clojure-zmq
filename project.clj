654b815f6db3e6f9d1db9cebc21793f1a0ebb4b1
Michael Stephens <michaeljste@gmail.com>
2010-08-06 00:39:59 -0400

    Leiningen project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojure-zmq "2.0.7-SNAPSHOT"
+  :description "A Clojure binding/wrapper for ZeroMQ"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [org.clojars.mikejs/jzmq "2.0.7-SNAPSHOT"]])
